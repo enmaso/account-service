3266c7cce4504757709aecfc5a599f015a622472
Adam Jones <adam@enmaso.com>
2017-08-05 13:47:29 -0700

    Removed comments
    Removed comments

--- src/routes/authenticate.js
+++ src/routes/authenticate.js
@@ -49,87 +49,8 @@ const router = Router()
 router.post('/', passport.authenticate('local'), (req, res) => {
   res.send({
     account: req.user,
-    session: req.session 
+    session: req.session
   })
 })
-
-/*
-router.post('/', (req, res) => {
-  let params = _.pick(req.body, 'email', 'password')
-  if(!params.email || !params.password) {
-    // Missing email and/or password fields
-    logger.error('Missing parameters')
-    res.status(400).json({
-      success: false,
-      message: 'Missing parameters'
-    })
-  } else if(!validator.isEmail(params.email) || !validator.isLength(params.password, 4)) {
-    // Invalid email and/or password
-    logger.warn('Invalid email and/or password')
-    res.status(400).json({
-      success: false,
-      message: 'Invalid email and/or password'
-    })
-  } else {
-    Account.findOne({
-      email: params.email
-    })
-    .then(account => {
-      if(account == null) {
-        // No account found
-        logger.warn('Account not found')
-        res.status(404).json({
-          success: false,
-          message: 'Account not found'
-        })
-      } else if(!Account.match(params.password, account.password)) {
-        // Account found, but invalid password
-        logger.warn('Invalid password')
-        res.status(400).json({
-          success: false,
-          message: 'Invalid password'
-        })
-      } else {
-
-
-
-        // Account found and authenticated
-        let exp = new Date(Date.now() + 2592000)
-        let key = account.key
-
-
-
-        // Create session
-
-        let payload = {
-          sub: account._id,
-          email: account.email
-        }
-        let token = jwt.sign(payload, account.key)
-        let opts = {
-          expires: new Date(Date.now() + 2592000),
-          httpOnly: true,
-          secure: process.env.NODE_ENV == 'production'
-        }
-        res.cookie('token', token, opts)
-        res.status(200).json({
-          success: true,
-          account: account
-        })
-
-
-
-      }
-    })
-    .catch(err => {
-      logger.error(err)
-      res.status(500).json({
-        success: false,
-        message: 'Internal Server Error'
-      })
-    })
-  }
-})
-*/
 
 export default router
